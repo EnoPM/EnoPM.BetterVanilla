f0498683c965386ba9dcf263039c09253f8631b2
EnoPM <marwane.rhaiem@gmail.com>
2026-01-16 11:52:33 +0100

    Remove Build project + update features-registry.json

--- BetterVanilla.Build/Core/Interfaces/IConsoleConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BetterVanilla.Build.Core.Interfaces;
-
-public interface IConsoleConfig
-{
-    public bool Verbose { get; }
-}
--- BetterVanilla.Build/Core/Interfaces/IIl2CppAutoInteropConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BetterVanilla.Build.Core.Interfaces;
-
-public interface IIl2CppAutoInteropConfig
-{
-    public string Il2CppAutoInteropVersion { get; }
-    
-    public string Il2CppAutoInteropRuntime { get; }
-}
